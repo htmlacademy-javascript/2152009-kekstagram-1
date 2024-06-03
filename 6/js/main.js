e3ff3224de413a16f3b01c7ab7c46f4efc9914ac
Keks <keks@htmlacademy.ru>
2024-06-03 17:05:58 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,4 @@
+import {renderPhotoUsers} from './renderingThumbnails.js';
+import './renderingBigPicture.js';
+
+renderPhotoUsers();
---
+++ 6/js/util.js
@@ -0,0 +1,20 @@
+export const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+export function createRandomIdFromRangeGenerator (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+export const isEscapeKey = (evt) => evt.key === 'Escape';
+export const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
