c3ba4f01e94a9498f20be0054b0e06d9ca67bf83
Эльвира <45ark97@gmail.com>
2024-06-06 17:44:57 +0500

    8.15. Открывается и закрывается (часть 2)

--- js/renderingBigPicture.js
+++ js/renderingBigPicture.js
@@ -1,6 +1,6 @@
 import { isEscapeKey } from './util.js';
 import { similarPhoto } from './data.js';
-
+import {renderCommentsByStep} from './commentsBigPicture.js';
 const bigPictureModal = document.querySelector('.big-picture');
 const usersPhotoContainer = document.querySelector('.pictures');
 const pictureUrl = bigPictureModal.querySelector('.big-picture__img');
@@ -9,48 +9,50 @@ const commentsCount = bigPictureModal.querySelector('.comments-count');
 const canselBigPictureButton = bigPictureModal.querySelector(
   '.big-picture__cancel'
 );
-const socialCommentCount = bigPictureModal.querySelector(
-  '.social__comment-count'
-);
-const commentsLoader = bigPictureModal.querySelector('.comments-loader');
+
 const socialComments = document.querySelector('.social__comments');
 const commentTemplate = document.querySelector('#comments').content.querySelector('.social__comment');
 const commentsListFragment = document.createDocumentFragment();
+const commentsLoader = bigPictureModal.querySelector('.comments-loader');
+socialComments.innerHTML = '';
 
-const renderComments = (comments) => {
-  socialComments.innerHTML = '';
+export const renderComments = (comments) => {
+
+
   comments.forEach(({ avatar, message, name }) => {
+
     const commentElement = commentTemplate.cloneNode(true);
     commentElement.querySelector('.social__picture').src = avatar;
     commentElement.querySelector('.social__picture').alt = name;
     commentElement.querySelector('.social__text').innerHTML = message;
     commentsListFragment.appendChild(commentElement);
+
+
   });
   socialComments.appendChild(commentsListFragment);
 };
-
 const openBigPictureModal = (evt) => {
   const objectId = evt.target.parentElement.id;
   const objectById = similarPhoto.find((x) => x.id === Number(objectId));
   if (typeof(objectById) === 'undefined'){
     return;
   }
-
+  socialComments.innerHTML = '';
+  commentsLoader.classList.remove('hidden');
   bigPictureModal.classList.remove('hidden');
-  socialCommentCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
   document.querySelector('body').classList.add('modal-open');
   pictureUrl.querySelector('img').src = objectById.url;
   likesCount.textContent = objectById.likes;
   commentsCount.textContent = objectById.comments.length;
   bigPictureModal.querySelector('.social__caption').innerHTML =
     objectById.description;
-
-  renderComments(objectById.comments);
+  renderCommentsByStep(objectById.comments);
 };
 const closeBigPictureModal = () => {
   bigPictureModal.classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
+
+
 };
 export const initBigPicture = ()=>{
   usersPhotoContainer.addEventListener('click', (evt) => {
@@ -59,6 +61,7 @@ export const initBigPicture = ()=>{
 
   canselBigPictureButton.addEventListener('click', () => {
     closeBigPictureModal();
+
   });
 
   document.addEventListener('keydown', (evt) => {
