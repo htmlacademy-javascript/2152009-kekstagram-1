0f796141e9418747df7c154757e86c8db9123ce9
Эльвира <45ark97@gmail.com>
2024-06-14 18:11:53 +0500

    Сырая версия

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import {renderPhotoUsers} from './renderingThumbnails.js';
 import {initBigPicture} from './renderingBigPicture.js';
-
+import './newPictureForm.js';
 renderPhotoUsers();
 initBigPicture();
---
+++ js/newPictureForm.js
@@ -0,0 +1,65 @@
+import { isEscapeKey } from './util.js';
+
+const pictureUploadInput = document.querySelector('#upload-file');
+const pictureUploadOverlay = document.querySelector('.img-upload__overlay');
+const pictureUploadForm = document.querySelector('.img-upload__form');
+const canselPictureUploadButton = document.querySelector('#upload-cancel');
+const pictureUploadFormSubmit = document.querySelector('.img-upload__submit');
+const inputHashtag = document.querySelector('.text__hashtags');
+const pictureDescription = document.querySelector('.text__description');
+const pristine = new Pristine(pictureUploadForm);
+pristine.addValidator(inputHashtag, (value) => {
+  const hashtagRegex = /^(#[a-zA-ZА-ЯЁа-яё0-9]{1,19}(?:\s|$)){0,5}$/;
+  const isValid = hashtagRegex.test(value.trim());
+  const hashtags = value.toLowerCase().split(/\s+/);
+  const uniqueHashtags = new Set(hashtags);
+  return isValid && hashtags.length === uniqueHashtags.size;
+}, 'Хэштег должен соответствовать регулярному выражению: /^(#[a-zA-ZА-ЯЁа-яё0-9]{1,19}(?:\s|$)){0,5}$/ и быть уникальным.Допускается максимальное число хэштегов: 5');
+
+pristine.addValidator(pictureDescription, (value) => value.length <= 140, 'Максимум 140 символов');
+let onDocumentKeydown;
+const validatePictureForm = () => {
+  pictureUploadForm.addEventListener('input', (evt) => {
+    evt.preventDefault();
+    const isValid = pristine.validate();
+    pictureUploadFormSubmit.disabled = !isValid;
+  });
+};
+
+const closePictureUploadModal = () => {
+  pictureUploadOverlay.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+  pictureUploadInput.value = '';
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+const openPictureUploadModal = () => {
+  pictureUploadOverlay.classList.remove('hidden');
+
+  document.querySelector('body').classList.add('modal-open');
+  onDocumentKeydown = (event) => {
+    const activeElement = document.activeElement;
+    if (isEscapeKey(event) && activeElement !== inputHashtag && activeElement !== pictureDescription) {
+      closePictureUploadModal();
+      event.stopPropagation();
+    }
+  };
+
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+pictureUploadInput.addEventListener('change', () => {
+  openPictureUploadModal();
+});
+
+canselPictureUploadButton.addEventListener('click', () => {
+  closePictureUploadModal();
+});
+pictureUploadForm.addEventListener('submit', () => {
+  validatePictureForm();
+});
+
+pictureUploadForm.addEventListener('input',()=>{
+  validatePictureForm();
+
+});
