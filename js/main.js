73d3696ab90eb9538dd1b65281ba879dd7eb2a32
Эльвира <45ark97@gmail.com>
2024-06-24 15:55:58 +0500

    Итог

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 import {renderPhotoUsers} from './renderingThumbnails.js';
 import {initBigPicture} from './renderingBigPicture.js';
 import {newPictureForm} from './newPictureForm.js';
-import { validatePictureForm } from './pictureFormValidation.js';
+import { initPictureFormValidation } from './pictureFormValidation.js';
 
 renderPhotoUsers();
 initBigPicture();
 newPictureForm();
-validatePictureForm();
+initPictureFormValidation();
--- js/pictureFormValidation.js
+++ js/pictureFormValidation.js
@@ -11,37 +11,37 @@ const pristine = new Pristine(pictureUploadForm, {
 });
 
 
-const initPictureFromValidation = ()=>{
+export const initPictureFormValidation = ()=>{
 
 
   pristine.addValidator(inputHashtag, (value) => {
     const hashtags = value.trim().split(/\s+/).filter(Boolean);
     return hashtags.every((tag) => tag.startsWith('#'));
-  }, 'Хэш-тег должен начинаться с символа #', 1,true);
+  }, 'Хэш-тег должен начинаться с символа #', 6,true);
   pristine.addValidator(inputHashtag, (value) => {
     const hashtags = value.trim().split(/\s+/).filter(Boolean);
     return hashtags.every((tag) => tag.length > 1);
-  }, 'Хэш-тег не может состоять только из одной решётки',2,true);
+  }, 'Хэш-тег не может состоять только из одной решётки',5,true);
   pristine.addValidator(inputHashtag, (value) => {
     const hashtags = value.trim().split(/\s+/).filter(Boolean);
     const hashtagRegex = /^#[a-zA-ZА-ЯЁа-яё0-9]+$/;
     return hashtags.every((tag) => hashtagRegex.test(tag));
-  }, 'Строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы, символы пунктуации и т. д.',3,true);
+  }, 'Строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы, символы пунктуации и т. д.',4,true);
   pristine.addValidator(inputHashtag, (value) => {
     const hashtags = value.trim().split(/\s+/).filter(Boolean);
     return hashtags.every((tag) => tag.length <= 20);
-  }, 'Максимальная длина одного хэш-тега 20 символов, включая решётку',4,true);
+  }, 'Максимальная длина одного хэш-тега 20 символов, включая решётку',3,true);
   pristine.addValidator(inputHashtag, (value) => {
     const hashtags = value.trim().split(/\s+/).filter(Boolean);
     const containsSpaces = value.trim().includes(' ');
     return (hashtags.length <= 1 || containsSpaces);
-  }, 'Хэш-теги разделяются пробелами',5,true);
+  }, 'Хэш-теги разделяются пробелами',2,true);
 
   pristine.addValidator(inputHashtag, (value) => {
     const hashtags = value.trim().toLowerCase().split(/\s+/).filter(Boolean);
     const uniqueHashtags = new Set(hashtags);
     return hashtags.length === uniqueHashtags.size;
-  }, 'Один и тот же хэш-тег не может быть использован дважды',6,true);
+  }, 'Один и тот же хэш-тег не может быть использован дважды',1,true);
 
   pristine.addValidator(inputHashtag, (value) => {
     const hashtags = value.trim().split(/\s+/).filter(Boolean);
@@ -61,6 +61,4 @@ const initPictureFromValidation = ()=>{
     pictureUploadFormSubmit.disabled = !isValid;
   });
 };
-export const validatePictureForm = () => {
-  initPictureFromValidation();
-};
+
