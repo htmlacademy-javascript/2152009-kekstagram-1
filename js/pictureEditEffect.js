83b65f61bbc9edad35f56bbcaf118e4f00604d74
Эльвира <45ark97@gmail.com>
2024-06-28 16:16:00 +0500

    Улучшение кода

--- js/pictureEditEffect.js
+++ js/pictureEditEffect.js
@@ -1,40 +1,110 @@
 const effectLevelSlider = document.querySelector('.effect-level__slider');
-const imgUploadPreview = document.querySelector('.img-upload__preview');
-
+const pictureUploadPreview = document.querySelector('.img-upload__preview');
+const picture = pictureUploadPreview .querySelector('img');
 const imgUploadEffects = document.querySelector('.img-upload__effects');
 const effectLevelValue = document.querySelector('.effect-level__value');
 const imgUploadEffectLevel = document.querySelector(
   '.img-upload__effect-level'
 );
+const customizationEffects = (effect)=>{
+  const effectLevelObject = {
+    chrome:{
+      range: {
+        min: 0,
+        max: 1,
+      },
+      start: 1,
+      step: 0.1,
+    },
+    sepia:{
+      range: {
+        min: 0,
+        max: 1,
+      },
+      start: 1,
+      step: 0.1,
+    },
+    marvin:{
+      range: {
+        min: 0,
+        max: 100,
+      },
+      start: 100,
+      step: 1,
+    },
+    phobos:{
+      range: {
+        min: 0,
+        max: 3,
+      },
+      start: 3,
+      step: 0.1,
+    },
+    heat:{
+      range: {
+        min: 1,
+        max: 3,
+      },
+      start: 3,
+      step: 0.1,
+    }
+
+  };
+  picture.classList.remove(
+    'effects__preview--chrome',
+    'effects__preview--sepia',
+    'effects__preview--marvin',
+    'effects__preview--phobos',
+    'effects__preview--heat'
+  );
+  picture.classList.add(`effects__preview--${effect}`);
+  effectLevelSlider.noUiSlider.updateOptions(effectLevelObject[effect]);
+  effectLevelSlider.classList.remove('hidden');
+  imgUploadEffectLevel.classList.remove('hidden');
+};
 
 const applyEffect = (value) => {
-  const currentEffect = imgUploadPreview.className;
+  const currentEffect = picture.className;
   switch (currentEffect) {
     case 'img-upload__preview effects__preview--chrome':
-      imgUploadPreview.style.filter = `grayscale(${value})`;
+      picture.style.filter = `grayscale(${value})`;
       break;
 
     case 'img-upload__preview effects__preview--sepia':
-      imgUploadPreview.style.filter = `sepia(${value})`;
+      picture.style.filter = `sepia(${value})`;
       break;
 
     case 'img-upload__preview effects__preview--marvin':
-      imgUploadPreview.style.filter = `invert(${value}%)`;
+      picture.style.filter = `invert(${value}%)`;
       break;
 
     case 'img-upload__preview effects__preview--phobos':
-      imgUploadPreview.style.filter = `blur(${value}px)`;
+      picture.style.filter = `blur(${value}px)`;
       break;
 
     case 'img-upload__preview effects__preview--heat':
-      imgUploadPreview.style.filter = `brightness(${value})`;
+      picture.style.filter = `brightness(${value})`;
       break;
 
     default:
-      imgUploadPreview.style.filter = '';
+      picture.style.filter = '';
       break;
   }
 };
+export const resetEditingEffect = () => {
+  picture.className = 'img-upload__preview none';
+  picture.style.filter = '';
+  picture.classList.remove(
+    'effects__preview--chrome',
+    'effects__preview--sepia',
+    'effects__preview--marvin',
+    'effects__preview--phobos',
+    'effects__preview--heat'
+  );
+  effectLevelSlider.classList.add('hidden');
+  imgUploadEffectLevel.classList.add('hidden');
+};
+
 export const initPictureEditEffect = () => {
   noUiSlider.create(effectLevelSlider, {
     range: {
@@ -49,139 +119,12 @@ export const initPictureEditEffect = () => {
   });
   imgUploadEffects.addEventListener('click', (evt) => {
     const effect = evt.target.value;
-    imgUploadPreview.className = 'img-upload__preview';
-    switch (effect) {
-      case 'chrome': {
-        imgUploadPreview.classList.remove(
-          'effects__preview--chrome',
-          'effects__preview--sepia',
-          'effects__preview--marvin',
-          'effects__preview--phobos',
-          'effects__preview--heat'
-        );
-        imgUploadPreview.classList.add('effects__preview--chrome');
-        effectLevelSlider.classList.remove('hidden');
-        imgUploadEffectLevel.classList.remove('hidden');
-        effectLevelSlider.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 1,
-          },
-          start: 1,
-          step: 0.1,
-        });
-        break;
-      }
-      case 'sepia': {
-        imgUploadPreview.classList.remove(
-          'effects__preview--chrome',
-          'effects__preview--sepia',
-          'effects__preview--marvin',
-          'effects__preview--phobos',
-          'effects__preview--heat'
-        );
-        imgUploadPreview.classList.add('effects__preview--sepia');
-        effectLevelSlider.classList.remove('hidden');
-        imgUploadEffectLevel.classList.remove('hidden');
-        effectLevelSlider.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 1,
-          },
-          start: 1,
-          step: 0.1,
-        });
-        break;
-      }
-      case 'marvin': {
-        imgUploadPreview.classList.remove(
-          'effects__preview--chrome',
-          'effects__preview--sepia',
-          'effects__preview--marvin',
-          'effects__preview--phobos',
-          'effects__preview--heat'
-        );
-        imgUploadPreview.classList.add('effects__preview--marvin');
-        effectLevelSlider.classList.remove('hidden');
-        imgUploadEffectLevel.classList.remove('hidden');
-        effectLevelSlider.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 100,
-          },
-          start: 100,
-          step: 1,
-        });
-        break;
-      }
-      case 'phobos': {
-        imgUploadPreview.classList.remove(
-          'effects__preview--chrome',
-          'effects__preview--sepia',
-          'effects__preview--marvin',
-          'effects__preview--phobos',
-          'effects__preview--heat'
-        );
-        imgUploadPreview.classList.add('effects__preview--phobos');
-        effectLevelSlider.classList.remove('hidden');
-        imgUploadEffectLevel.classList.remove('hidden');
-        effectLevelSlider.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 3,
-          },
-          start: 3,
-          step: 0.1,
-        });
-        break;
-      }
-      case 'heat': {
-        imgUploadPreview.classList.remove(
-          'effects__preview--chrome',
-          'effects__preview--sepia',
-          'effects__preview--marvin',
-          'effects__preview--phobos',
-          'effects__preview--heat'
-        );
-        imgUploadPreview.classList.add('effects__preview--heat');
-        effectLevelSlider.classList.remove('hidden');
-        imgUploadEffectLevel.classList.remove('hidden');
-        effectLevelSlider.noUiSlider.updateOptions({
-          range: {
-            min: 1,
-            max: 3,
-          },
-          start: 3,
-          step: 0.1,
-        });
-        break;
-      }
-      case 'none': {
-        imgUploadPreview.classList.remove(
-          'effects__preview--chrome',
-          'effects__preview--sepia',
-          'effects__preview--marvin',
-          'effects__preview--phobos',
-          'effects__preview--heat'
-        );
-        effectLevelSlider.classList.add('hidden');
-        imgUploadEffectLevel.classList.add('hidden');
-        imgUploadPreview.style.filter = '';
-        break;
-      }
+    picture.className = 'img-upload__preview';
+    if (effect !== 'none'){
+      customizationEffects(effect);
+    }else{
+      resetEditingEffect();
     }
   });
 };
-export const resetEditingEffect = () => {
-  imgUploadPreview.className = 'img-upload__preview none';
-  imgUploadPreview.style.filter = '';
-  imgUploadPreview.classList.remove(
-    'effects__preview--chrome',
-    'effects__preview--sepia',
-    'effects__preview--marvin',
-    'effects__preview--phobos',
-    'effects__preview--heat'
-  );
-  effectLevelSlider.classList.add('hidden');
-  imgUploadEffectLevel.classList.add('hidden');
-};
+
